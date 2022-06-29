ab7011d5393749df904fedefe535b8ce67b1f71b
Onurmk99 <onurmkoprulu@gmail.com>
2022-06-29 17:03:48 +0300

    Frawork is ready for features

--- src/test/java/com/xfleet/runners/CukesRunner.java
+++ src/test/java/com/xfleet/runners/CukesRunner.java
@@ -1,4 +1,33 @@
 package com.xfleet.runners;
 
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "pretty",
+                "html:target/reports/cucumber-report.html",
+                "json:target/reports/cucumber-report.json",
+                "rerun:target/reports/cucumber-report.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",// content root
+        glue = "com/xfleet/step_definitions", // source root
+        dryRun= false,
+        tags= "",
+        publish = false
+
+
+
+)
+
+
+
 public class CukesRunner {
+
+
 }
--- src/test/java/com/xfleet/utilities/BrowserUtils.java
+++ src/test/java/com/xfleet/utilities/BrowserUtils.java
@@ -1,4 +1,21 @@
 package com.xfleet.utilities;
 
 public class BrowserUtils {
+
+
+   // static sleep method accept seconds and wait during time
+   //@param seconds
+    public static void sleep(int seconds) {
+
+        int wait =seconds*1000;
+        try {
+            Thread.sleep(wait);
+        } catch (InterruptedException e) {
+            System.out.println("static sleep issue");
+        }
+    }
+
+
+
+
 }
