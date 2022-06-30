3992250de26fd0c7ca219829be9aae2e67b026cf
Onurmk99 <onurmkoprulu@gmail.com>
2022-06-30 16:12:17 +0300

    Frawork is ready  last version1

--- src/test/java/com/xfleet/utilities/BrowserUtils.java
+++ src/test/java/com/xfleet/utilities/BrowserUtils.java
@@ -1,5 +1,12 @@
 package com.xfleet.utilities;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BrowserUtils {
 
 
@@ -15,7 +22,103 @@ public class BrowserUtils {
         }
     }
 
+    public static List<String> truckDriversUserName(){
+
+        String path = "credentials.xlsx";
+
+        List<String> truckDrivers = new ArrayList<>();
 
 
 
-}
+        try{
+            FileInputStream file = new FileInputStream(path);
+
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+            XSSFSheet sheet = workbook.getSheet("data");
+
+            int rowNumber = sheet.getPhysicalNumberOfRows();
+
+            for (int i = 1; i < rowNumber; i++) {
+
+                truckDrivers.add(sheet.getRow(i).getCell(0).toString());
+            }
+
+
+
+        } catch (Exception e){
+
+            e.printStackTrace();
+        }
+
+        return truckDrivers;
+    }
+
+    public static List<String> storeManagersUserName(){
+
+        String path = "credentials.xlsx";
+
+
+        List<String> storeManagers = new ArrayList<>();
+
+
+
+        try{
+            FileInputStream file = new FileInputStream(path);
+
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+            XSSFSheet sheet = workbook.getSheet("data");
+
+            int rowNumber = sheet.getPhysicalNumberOfRows();
+
+            for (int i = 1; i < rowNumber; i++) {
+
+                storeManagers.add(sheet.getRow(i).getCell(1).toString());
+            }
+
+
+        } catch (Exception e){
+
+            e.printStackTrace();
+        }
+        return storeManagers;
+    }
+
+    public static List<String> salesManagersUserName(){
+
+        String path = "credentials.xlsx";
+
+
+        List<String> salesManagers = new ArrayList<>();
+
+
+        try{
+            FileInputStream file = new FileInputStream(path);
+
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+            XSSFSheet sheet = workbook.getSheet("data");
+
+            int rowNumber = sheet.getPhysicalNumberOfRows();
+
+
+
+            for (int i = 1; i < rowNumber; i++) {
+
+                salesManagers.add(sheet.getRow(i).getCell(2).toString());
+            }
+
+
+
+        } catch (Exception e){
+
+            e.printStackTrace();
+        }
+
+        return salesManagers;
+    }
+    }
+
+
+
